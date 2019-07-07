6926cb2771b59ffa304f53ca2cba253bb03a4e43
nkihrk <40584391+nkihrk@users.noreply.github.com>
2019-07-08 03:47:09 +0900

    created the base of parent-related css

--- js/common/common.js
+++ js/common/common.js
@@ -1,3 +1,4 @@
+// Loading Gif. Use promise to express rich loading page.
 $(function () {
     h = $(window).height();
     $('#container').css('display', 'none');
@@ -8,4 +9,19 @@ $(window).on('load', function () {
     $('#loader-bg').delay(900).fadeOut(800);
     $('#loading').delay(600).fadeOut(300);
     $('#container').css('display', 'block');
+});
+
+// common settings
+$(function () {
+    // Insert a window height to .menu and .content
+    h = $(window).height();
+    menu = document.getElementsByClassName('menu');
+    content = document.getElementsByClassName('content');
+    $(menu).css('height', h + 'px');
+    $(content).css('height', h + 'px');
+
+    // Insert a copyright-date into footer
+    copyright = document.getElementById('copyright');
+    date = new Date().getFullYear();
+    copyright.innerHTML = date + ' &copy; NkiHrk';
 });
--- js/index/main.js
+++ js/index/main.js
@@ -1,19 +1,28 @@
-// Get a window height and apply to the class container
-$(document).ready(function () {
-    hsize = $(window).height();
-    $('#container').css('min-height', hsize + 'px');
-});
+// // Get a window height and apply to the class container
+// $(document).ready(function () {
+//     hsize = $(window).height();
+//     $('#container').css('min-height', hsize + 'px');
+// });
 
-$(window).resize(function () {
-    hsize = $(window).height();
-    $('#container').css('min-height', hsize + 'px');
-});
+// $(window).resize(function () {
+//     hsize = $(window).height();
+//     $('#container').css('min-height', hsize + 'px');
+// });
 
-// Set a appropriate height to the class content-box
-$(function () {
-    h = $(window).height();
-    contentBox = document.getElementsByClassName('content-box');
-    offset = $(contentBox).offset().top;
-    targetHeight = h - offset;
-    $(contentBox).css('min-height', targetHeight + 'px');
-});
+// // Set a appropriate height to the class content-box
+// $(function () {
+//     h = $(window).height();
+//     contentBox = document.getElementsByClassName('content-box');
+//     offset = $(contentBox).offset();
+//     top = $(offset).top;
+//     targetHeight = h - top;
+//     $(contentBox).css('min-height', targetHeight + 'px');
+// });
+
+// // Prefix of the bluring edges
+// $(function () {
+//     w = $(window).width();
+//     h = $(window).height();
+//     container = document.getElementById('container');
+//     width = 
+// });
