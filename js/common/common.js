ff672d4a432f3a8168b636f8708532ef7dce35c5
nkihrk <40584391+nkihrk@users.noreply.github.com>
2019-07-08 15:45:33 +0900

    bluring background image

--- js/common/common.js
+++ js/common/common.js
@@ -1,27 +1,72 @@
-// Loading Gif. Use promise to express rich loading page.
-$(function () {
-    h = $(window).height();
-    $('#container').css('display', 'none');
-    $('#loader-bg, #loader').height(h).css('display', 'block');
-});
+;
+(function (window, $) {
+    //Variable
+    let windowHeight = $(window).height();
 
-$(window).on('load', function () {
-    $('#loader-bg').delay(900).fadeOut(800);
-    $('#loading').delay(600).fadeOut(300);
-    $('#container').css('display', 'block');
-});
+    $(function () {
+        // Loading Gif. Use promise to express rich loading page.
+        let loading = {
+            func: function () {
+                $('#container').css('display', 'none');
+                $('#loader-bg, #loader').height(windowHeight).css('display', 'block');
+            },
+        }
+        loading.func();
 
-// common settings
-$(function () {
-    // Insert a window height to .menu and .content
-    h = $(window).height();
-    menu = document.getElementsByClassName('menu');
-    content = document.getElementsByClassName('content');
-    $(menu).css('height', h + 'px');
-    $(content).css('height', h + 'px');
 
-    // Insert a copyright-date into footer
-    copyright = document.getElementById('copyright');
-    date = new Date().getFullYear();
-    copyright.innerHTML = date + ' &copy; NkiHrk';
-});
+        // Insert a window height to .menu and .content
+        let common = {
+            func: function () {
+                $(this.menu).css('height', windowHeight + 'px');
+                $(this.content).css('height', windowHeight + 'px');
+            },
+            menu: document.getElementsByClassName('menu'),
+            content: document.getElementsByClassName('content')
+        };
+        common.func();
+
+
+        // Insert copyright into footer
+        let year = new Date().getFullYear();
+        let copyright = {
+            func: function () {
+                this.id.appendChild(this.text);
+            },
+            id: document.getElementById('copyright'),
+            text: document.createTextNode(year + ' © NkiHrk'),
+        };
+        copyright.func();
+
+
+        // Circlized + Gradation
+        let circlized = {
+            func: function () {            
+                let wrapperWidth = this.width + this.thickness;
+                let imageTop = wrapperWidth / 2.0 - this.width / 2.0;
+
+                $(this.wrapper).css({
+                    'width': wrapperWidth + 'px',
+                    'height': wrapperWidth + 'px'
+                });
+                $(this.image).css({
+                    'width': this.width + 'px',
+                    'top': imageTop + 'px',
+                    'left': imageTop + 'px'
+                });
+            },
+            wrapper: document.getElementsByClassName('wrapper-gradation-circlized circlized'),
+            image: document.getElementsByClassName('gradation circlized'),
+            width: $.circlized.width,
+            thickness: $.circlized.thickness,
+        };
+        circlized.func();
+
+
+    });
+
+    $(window).on('load', function () {
+        $('#loader-bg').delay(900).fadeOut(800);
+        $('#loading').delay(600).fadeOut(300);
+        $('#container').css('display', 'block');
+    });
+})(window, jQuery);
