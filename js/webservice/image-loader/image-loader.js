0296da635d3585b85a9e1aa88a0c87bab036c5a2
nkihrk <40584391+nkihrk@users.noreply.github.com>
2019-08-08 01:23:10 +0900

    added seamless drag event

--- js/webservice/image-loader/image-loader.js
+++ js/webservice/image-loader/image-loader.js
@@ -37,25 +37,25 @@
                     // let imgStyle = 'position: absolute; width: 30%;';
 
 
-                    if (dropImage.flag == 0) {
-                        reader.onload = function (e) {
+                    // if (dropImage.flag == 0) {
+                    reader.onload = function (e) {
 
-                            dropImage.id += 1;
-                            dropImage.flag = 0;
-                            // let assertImg = '<img id ="' + dropImage.id + '" style="' + imgStyle + '">';
-                            let assertImg = '<div id="test_box" style="position: absolute; width: 30%;"><img id ="' + dropImage.id + '" style="width: 100%;"></div>';
-                            // $('#image').prepend(assertImg);
-                            $('#image').append(assertImg);
-                            const id = dropImage.id;
-                            // console.log(id);
-                            const img = document.getElementById(id);
-                            img.src = e.target.result;
-                            // console.log('done reader');
+                        dropImage.id += 1;
+                        dropImage.flag = 0;
+                        // let assertImg = '<img id ="' + dropImage.id + '" style="' + imgStyle + '">';
+                        let assertImg = '<div class="img-style"><img id ="' + dropImage.id + '" style="width: 100%;"></div>';
+                        // $('#image').prepend(assertImg);
+                        $('#image').append(assertImg);
+                        const id = dropImage.id;
+                        // console.log(id);
+                        const img = document.getElementById(id);
+                        img.src = e.target.result;
+                        // console.log('done reader');
 
-                        }
-                        reader.readAsDataURL(file);
-                        // console.log('done');
                     }
+                    reader.readAsDataURL(file);
+                    // console.log('done');
+                    // }
 
 
                     // Get mouse pos in real-time
@@ -63,10 +63,7 @@
                         let mouseClientX = e.clientX;
                         let mouseClientY = e.clientY;
                         const imgId = '#' + dropImage.id;
-                        // const $img = $(imgId);
-                        const $img = $('#test_box');
-                        // const imgWidth = $(imgId).width();
-                        // const imgHeight = $(imgId).height();
+                        const $img = $(imgId).parent();
                         const imgWidth = $img.width();
                         const imgHeight = $img.height();
 
@@ -77,7 +74,6 @@
                                 'top': mouseClientY - imgHeight / 2 + 'px',
                                 'left': mouseClientX - imgWidth / 2 + 'px'
                             });
-                            // $img.wrap('<div id="test_box" style="position: absolute; width: 30%;"></div>');
                             dropImage.flag = 1;
                         }
                     });
@@ -89,48 +85,47 @@
 
 
             const move = function () {
-                console.log('move()');
+                // console.log('move()');
 
-                //ドラッグフラグ
                 var drag_flg = false;
-                //マウスダウンの位置
+
                 var pos1;
-                var pos2;
-                //要素位置の修正値
+
                 var posX1;
                 var posY1;
 
-                //要素内でマウスボタンが押された場合
-                $(document).on('mousedown', '#test_box', function (evt1) {
-                    console.log('move()');
+                var $imgId;
 
-                    //ドラッグ判定（ドラッグしてない場合）
+                $(document).on('mousedown', '.img-style', function (evt1) {
+                    // console.log('move()');
+
                     if (drag_flg == false) {
 
-                        //要素の位置取得
-                        pos1 = $("#test_box").position();
-                        //要素位置を取得して修正値を計算
+                        $imgId = $(this);
+
+                        pos1 = $imgId.position();
+                        // console.log($imgId);
+
                         posX1 = evt1.clientX - pos1.left;
                         posY1 = evt1.clientY - pos1.top;
 
-                        //ドラッグ中にする
                         drag_flg = true;
+                    }
+                });
 
-                        //ドラッグ中の場合
-                    } else if (drag_flg == true) {
-
-                        //要素のドラッグを解除
+                $(document).on('mouseup', '.img-style', function (evt2) {
+                    if (drag_flg == true) {
                         drag_flg = false;
                     }
                 });
 
-                //ドキュメント上でマウスポインタが動いた場合
-                $(document).mousemove(function (evt2) {
-                    //ドラッグ中の場合
+                $(document).mousemove(function (evt3) {
+                    // Prevent the default drag event
+                    evt3.preventDefault();
+
                     if (drag_flg == true) {
-                        //要素位置をCSSで設定
-                        $("#test_box").css("left", (evt2.clientX - posX1));
-                        $("#test_box").css("top", (evt2.clientY - posY1));
+                        $imgId.css("left", (evt3.clientX - posX1));
+                        $imgId.css("top", (evt3.clientY - posY1));
                     }
                 });
             };
