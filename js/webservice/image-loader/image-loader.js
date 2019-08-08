e9568b2fc712214f326c12bf7bfe89a14787da0e
nkihrk <40584391+nkihrk@users.noreply.github.com>
2019-08-08 13:44:39 +0900

    implemented image-resizing

--- js/webservice/image-loader/image-loader.js
+++ js/webservice/image-loader/image-loader.js
@@ -96,16 +96,36 @@
                 // console.log('move()');
 
                 var drag_flg = false;
+
                 var pos1;
                 var posX1;
                 var posY1;
                 var $imgId;
 
+                // const resizeBoxPos = [
+                //     "leftTop",
+                //     "rightTop",
+                //     "rightBottom",
+                //     "leftBottom"
+                // ];
+                const resizeBox =
+                    '<div class="left-top"></div>' +
+                    '<div class="right-top"></div>' +
+                    '<div class="right-bottom"></div>' +
+                    '<div class="left-bottom"></div>';
+
                 $(document).on('mousedown', '.img-style', function (e) {
                     // console.log('move()');
 
-                    $('div').removeClass('selected');
-                    $(this).addClass('selected');
+                    $('div').removeClass('selected move-pointer');
+                    $('div').remove('.left-top');
+                    $('div').remove('.right-top');
+                    $('div').remove('.right-bottom');
+                    $('div').remove('.left-bottom');
+
+                    $(this).addClass('selected move-pointer');
+                    $(this).prepend(resizeBox);
+
 
                     if (drag_flg == false) {
 
@@ -113,28 +133,82 @@
                         $imgId.appendTo('#image');
 
                         pos1 = $imgId.position();
+
                         // console.log($imgId);
 
                         posX1 = e.clientX - pos1.left;
                         posY1 = e.clientY - pos1.top;
 
                         drag_flg = true;
+                        console.log('drag_flg true');
                     }
                 });
 
-                $(document).on('mouseup', '.img-style', function () {
-                    if (drag_flg == true) {
-                        drag_flg = false;
+                var down_flg = false;
+                var resize_flg = false;
+
+                var $target;
+                var targetPosX;
+                var targetPosY;
+
+
+                $(document).on('mousedown', '.right-bottom', function (e) {
+                    console.log('right-bottom detected');
+
+                    if (down_flg == false) {
+                        $target = $(this).parent();
+                        targetPos = $(this).parent().position();
+                        targetPosX = targetPos.left;
+                        targetPosY = targetPos.top;
+
+                        down_flg = true;
+                        resize_flg = true;
+                        console.log('down_flg true');
+                        console.log('resize_flg true');
                     }
                 });
+
+
+                $(document).mouseup(function () {
+                    if (drag_flg == true) {
+                        drag_flg = false;
+                        console.log('drag_flg false');
+
+                    }
+
+                    if (down_flg == true) {
+                        down_flg = false;
+                        console.log('down_flg false');
+
+                    }
+
+                    if (resize_flg == true) {
+                        resize_flg = false;
+                        console.log('resize_flg false');
+
+                    }
+                });
+
 
                 $(document).mousemove(function (e) {
                     // Prevent from the default drag event
                     e.preventDefault();
 
-                    if (drag_flg == true) {
-                        $imgId.css("left", (e.clientX - posX1));
-                        $imgId.css("top", (e.clientY - posY1));
+                    if (drag_flg == true && resize_flg == false) {
+                        $imgId.css('left', (e.clientX - posX1));
+                        $imgId.css('top', (e.clientY - posY1));
+                        console.log('drag one is called');
+
+                    }
+
+                    if (down_flg == true && resize_flg == true) {
+                        $target.css({
+                            'top': targetPosY + 'px',
+                            'left': targetPosX + 'px',
+                            'width': e.clientX - targetPosX + 'px',
+                        });
+                        console.log('down one is called');
+
                     }
                 });
             };
