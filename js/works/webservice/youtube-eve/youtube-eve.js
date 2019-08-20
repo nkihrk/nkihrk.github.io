344a3e83bfb6fbaf95a7dd7885d678ef785ce8c5
nkihrk <40584391+nkihrk@users.noreply.github.com>
2019-08-20 22:28:35 +0900

    tried to fix the tabName issue

--- js/works/webservice/youtube-eve/youtube-eve.js
+++ js/works/webservice/youtube-eve/youtube-eve.js
@@ -103,19 +103,19 @@
                                 videoID = url.split('v=')[1];
                                 videoID = videoID.split('&')[0];
                                 youtubeID = videoID + '?rel=0&showinfo=0';
-                                jsonYt = !config.youtube.API_KEY ? 0 : ajaxDataVideo(videoID);
+                                jsonYt = config.youtube.API_KEY == null ? -1 : ajaxDataVideo(videoID);
                                 isVideo = true;
                             } else {
                                 // Should be a movie or just a list without any options
                                 if (url.match(/list=/)) {
                                     listID = url.split('list=')[1];
                                     youtubeID = 'videoseries?list=' + listID;
-                                    jsonYt = ajaxDataPlaylistItem(listID);
+                                    jsonYt = config.youtube.API_KEY == null ? -1 : ajaxDataPlaylistItem(listID);
                                     isVideo = false;
                                 } else if (url.match(/v=/)) {
                                     videoID = url.split('v=')[1];
                                     youtubeID = url.split('v=')[1] + '?rel=0&showinfo=0';
-                                    jsonYt = !config.youtube.API_KEY ? 0 : ajaxDataVideo(videoID);
+                                    jsonYt = config.youtube.API_KEY == null ? -1 : ajaxDataVideo(videoID);
                                     isVideo = true;
                                 }
                             }
@@ -125,16 +125,8 @@
                             HIGHEST_Z_INDEX += 1;
 
                             // Get a specific json data from youtube via data api
-                            if (jsonYt == -1) tabName = 'The API Key is missing; the key is needed to show a name of the now-playing video properly.';
-                            jsonYt.done(function (jsonData) {
-                                if (isVideo) {
-                                    tabName = jsonData.items[0].snippet.title;
-                                } else {
-                                    tabName = 'Playlist : Enqueued ' + jsonData.pageInfo.totalResults + ' videos';
-                                }
-                                // console.log('tabName', tabName);
-
-
+                            if (jsonYt == -1) {
+                                tabName = 'The API Key is missing; the key is needed to show a name of the now-playing video properly.';
                                 iframeTag = '<iframe src="https://www.youtube.com/embed/' + youtubeID + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
                                 assertFile = '<div id="' + newFile.id + '" class="grab-pointer file-wrap" style="width: 700px; top: 33%; left: calc(50% - 350px); z-index:' + HIGHEST_Z_INDEX + ';">' +
                                     '<div class="function-wrapper">' +
@@ -164,7 +156,47 @@
                                     '</div>' +
                                     '</div>';
                                 $('#add-files').append(assertFile);
-                            });
+                            } else {
+                                jsonYt.done(function (jsonData) {
+                                    if (isVideo) {
+                                        tabName = jsonData.items[0].snippet.title;
+                                    } else {
+                                        tabName = 'Playlist : Enqueued ' + jsonData.pageInfo.totalResults + ' videos';
+                                    }
+                                    // console.log('tabName', tabName);
+                                    iframeTag = '<iframe src="https://www.youtube.com/embed/' + youtubeID + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+                                    assertFile = '<div id="' + newFile.id + '" class="grab-pointer file-wrap" style="width: 700px; top: 33%; left: calc(50% - 350px); z-index:' + HIGHEST_Z_INDEX + ';">' +
+                                        '<div class="function-wrapper">' +
+                                        '<div class="resize-wrapper"></div>' +
+                                        '<div class="rotate-wrapper"></div>' +
+                                        '<div class="flip-wrapper"></div>' +
+                                        '<div class="trash-wrapper"></div>' +
+                                        '</div>' +
+                                        '<div class="is-flipped youtube-wrapper" style="width: 100%;">' +
+                                        '<div class="tab-block-youtube" style="position: relative;">' +
+                                        '<div class="fix-top-border">' +
+                                        '<div class="ellipsis tab-youtube bold agency-fb">' +
+                                        tabName +
+                                        '</div>' +
+                                        '</div>' +
+                                        '</div>' +
+                                        '<div class="child-search-youtube" style="position: relative;">' +
+                                        '<div class="child-search-box-wrapper" style="position: relative;">' +
+                                        '<input value="' + url + '" class="ellipsis child-search-box-youtube" placeholder="Paste a URL of any YouTube videos here." type="text" spellcheck="false" style="position: relative;">' +
+                                        '<div class="backspace-icon"></div>' +
+                                        '</div>' +
+                                        '<div class="hover-shadow-single child-search-button-youtube" style="position: relative;"></div>' +
+                                        '</div>' +
+                                        '<div class="content-youtube">' +
+                                        iframeTag +
+                                        '</div>' +
+                                        '</div>' +
+                                        '</div>';
+                                    $('#add-files').append(assertFile);
+                                });
+                            }
+
+
                         } else {}
                         console.log('URL', url);
                     });
@@ -183,39 +215,42 @@
                                 videoID = url.split('v=')[1];
                                 videoID = videoID.split('&')[0];
                                 youtubeID = videoID + '?rel=0&showinfo=0';
-                                jsonYt = ajaxDataVideo(videoID);
+                                jsonYt = config.youtube.API_KEY == null ? -1 : ajaxDataVideo(videoID);
                                 isVideo = true;
                             } else {
                                 // Should be a movie or just a list without any options
                                 if (url.match(/list=/)) {
                                     listID = url.split('list=')[1];
                                     youtubeID = 'videoseries?list=' + listID;
-                                    jsonYt = !config.youtube.API_KEY ? 0 : ajaxDataPlaylistItem(listID);
+                                    jsonYt = config.youtube.API_KEY == null ? -1 : ajaxDataPlaylistItem(listID);
                                     isVideo = false;
                                 } else if (url.match(/v=/)) {
                                     videoID = url.split('v=')[1];
                                     youtubeID = url.split('v=')[1] + '?rel=0&showinfo=0';
-                                    jsonYt = !config.youtube.API_KEY ? 0 : ajaxDataVideo(videoID);
+                                    jsonYt = config.youtube.API_KEY == null ? -1 : ajaxDataVideo(videoID);
                                     isVideo = true;
                                 }
                             }
 
 
                             // Get a specific json data from youtube via data api
-                            if (jsonYt == -1) tabName = 'The API Key is missing; the key is needed to show a name of the now-playing video properly.';
-                            jsonYt.done(function (jsonData) {
-                                if (isVideo) {
-                                    tabName = jsonData.items[0].snippet.title;
-                                } else {
-                                    tabName = 'Playlist : Enqueued ' + jsonData.pageInfo.totalResults + ' videos';
-                                }
-                                console.log('tabName', tabName);
-                            });
-
+                            if (jsonYt == -1) {
+                                tabName = 'The API Key is missing; the key is needed to show a name of the now-playing video properly.';
+                            } else {
+                                jsonYt.done(function (jsonData) {
+                                    if (isVideo) {
+                                        tabName = jsonData.items[0].snippet.title;
+                                        console.log('tabName(isVideo=true)', tabName);
+                                        $(this).parents('.youtube-wrapper').find('.tab-youtube').text(tabName);
+                                    } else {
+                                        tabName = 'Playlist : Enqueued ' + jsonData.pageInfo.totalResults + ' videos';
+                                        console.log('tabName(isVideo=false)', tabName);
+                                        $(this).parents('.youtube-wrapper').find('.tab-youtube').text(tabName);
+                                    }
+                                });
+                            }
 
                             $(this).parents('.youtube-wrapper').find('iframe').attr('src', 'https://www.youtube.com/embed/' + youtubeID);
-                            var test = tabName;
-                            $(this).parents('.youtube-wrapper').find('.tab-youtube').text(test);
                         } else {}
                     });
                 };
