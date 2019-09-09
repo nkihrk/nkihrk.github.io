7b1f657d8576d5b02f06213f668afc9b5f86c27a
nkihrk <40584391+nkihrk@users.noreply.github.com>
2019-09-09 23:35:03 +0900

    working on three.js

--- js/works/webservice/canvas-eve/three-eve.js
+++ js/works/webservice/canvas-eve/three-eve.js
@@ -1,8 +1,42 @@
-var camera, scene, renderer;
-var geometry, material, mesh;
+var camera, scene, renderer, mesh, controls;
+var group;
+
+// Create default material
+// material = new THREE.MeshPhongMaterial();
 
 init();
 animate();
+
+// // file input button
+// document.getElementById('pickFile').addEventListener('change', openFile, false);
+
+// // file load
+// function openFile(evt) {
+
+//     var fileObject = evt.target.files[0];
+
+//     // delete previous objects from scene 
+//     while (group.children.length > 0) {
+//         group.remove(group.children[0]);
+//     }
+
+//     var reader = new FileReader();
+//     reader.onload = function () {
+//         var loader = new THREE.FBXLoader();
+//         // parse the .stl file
+//         var geometry = loader.parse(this.result);
+//         var mesh = new THREE.Mesh(geometry, material);
+//         mesh.castShadow = true;
+//         mesh.receiveShadow = true;
+//         scene.add(mesh);
+//         // group.add(mesh);
+//     };
+//     // --> update here 
+//     reader.readAsArrayBuffer(fileObject);
+// };
+
+// and the rest of my three.js code there : init() and animate() functions ...
+
 
 function init() {
 
@@ -16,6 +50,11 @@ function init() {
 
     mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
+
+    // // grid
+    // var gridHelper = new THREE.GridHelper(28, 28, 0x303030, 0x303030);
+    // gridHelper.position.set(0, -0.04, 0);
+    // scene.add(gridHelper);
 
     renderer = new THREE.WebGLRenderer({
         antialias: true,
