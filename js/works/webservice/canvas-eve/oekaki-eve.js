7aced667e5b5a97edc6bd4f5690649083f2113cb
nkihrk <40584391+nkihrk@users.noreply.github.com>
2019-09-26 05:02:39 +0900

    finished coding the UI of oekaki

--- js/works/webservice/canvas-eve/oekaki-eve.js
+++ js/works/webservice/canvas-eve/oekaki-eve.js
@@ -30,7 +30,9 @@ CANVASEVE.Oekaki = function (container) {
 CANVASEVE.Oekaki.prototype = {
     constructor: CANVASEVE.Oekaki,
 
-    options: {},
+    options: {
+        hue: 0,
+    },
 
 
     load: function () {
@@ -46,29 +48,16 @@ CANVASEVE.Oekaki.prototype = {
         var self = this;
 
         $(document).on(EVENTNAME_TOUCHSTART, '#color-oekaki', function () {
-            var centerX = self.centerX;
-            var centerY = self.centerY;
-            var minR = self.wheelInnerRadius;
-            var maxR = self.wheelRadius;
-            var mouseR = self.getDistance(centerX, centerY, clientX, clientY);
-
-            if (mouseR > minR && maxR > mouseR) {
+            var isWheelArea = self._isWheelArea();
+            if (isWheelArea) {
                 glFlgs.oekaki.move_wheelcircle_flg = true;
                 console.log('glFlgs.oekaki.move_wheelcircle_flg is ', glFlgs.oekaki.move_wheelcircle_flg);
             }
 
-
-            var mouseX = clientX - centerX;
-            var mouseY = clientY - centerY;
-            var minX = Math.cos(Math.PI * 2 / 3) * self.triangleRadius;
-            var maxX = self.triangleRadius;
-            var maxY = (-mouseX + maxX) / Math.sqrt(3);
-
-            if (mouseX > minX && maxX > mouseX) {
-                if (Math.abs(mouseY) >= 0 && maxY >= Math.abs(mouseY)) {
-                    glFlgs.oekaki.move_trianglecircle_flg = true;
-                    console.log('glFlgs.oekaki.move_trianglecircle_flg is ', glFlgs.oekaki.move_trianglecircle_flg);
-                }
+            var isTriangleArea = self._isTriangleArea();
+            if (isTriangleArea) {
+                glFlgs.oekaki.move_trianglecircle_flg = true;
+                console.log('glFlgs.oekaki.move_trianglecircle_flg is ', glFlgs.oekaki.move_trianglecircle_flg);
             }
         });
 
@@ -108,15 +97,15 @@ CANVASEVE.Oekaki.prototype = {
         var self = this;
 
         $(document).on(EVENTNAME_TOUCHSTART, '#color-oekaki', function () {
-            self._colorWheelArea(self);
-            self._colorTriangleArea(self);
+            self._colorWheelArea();
+            self._colorTriangleArea();
         });
 
         $(document).on(EVENTNAME_TOUCHMOVE, function () {
-            var originX = $(self.container).offset().left;
-            var originY = $(self.container).offset().top;
-            var centerX = originX + self.size / 2;
-            var centerY = originY + self.size / 2;
+            var originX = $(self.container).offset().left,
+                originY = $(self.container).offset().top,
+                centerX = originX + self.size / 2,
+                centerY = originY + self.size / 2;
             self.centerX = centerX;
             self.centerY = centerY;
 
@@ -137,9 +126,9 @@ CANVASEVE.Oekaki.prototype = {
 
 
     drawWheel: function () {
-        const resolution = 1;
-        const outerRadius = 100;
-        const innerRadius = outerRadius - this.wheelThickness / this.wheelRadius * 100;
+        const resolution = 1,
+            outerRadius = 100,
+            innerRadius = outerRadius - this.wheelThickness / this.wheelRadius * 100;
 
         var root = document.getElementById('color-wheel');
 
@@ -176,8 +165,8 @@ CANVASEVE.Oekaki.prototype = {
         }
     },
     _describeArc: function (x, y, radius, startAngle, endAngle) {
-        const start = this._polar2Cartesian(x, y, radius, endAngle);
-        const end = this._polar2Cartesian(x, y, radius, startAngle);
+        const start = this._polar2Cartesian(x, y, radius, endAngle),
+            end = this._polar2Cartesian(x, y, radius, startAngle);
 
         const arcSweep = endAngle - startAngle <= 180 ? '0' : '1';
 
@@ -204,18 +193,17 @@ CANVASEVE.Oekaki.prototype = {
 
 
     _createWheelCircle: function () {
-        var wheelCircle = document.createElement('div');
-        var r = this.wheelInnerRadius + this.wheelThickness / 2;
-        var theta = 0;
-        var left = r * Math.cos(theta + 3 / 2 * Math.PI) + this.size / 2;
-        var top = r * Math.sin(theta + 3 / 2 * Math.PI) + this.size / 2;
+        var wheelCircle = document.createElement('div'),
+            r = this.wheelInnerRadius + this.wheelThickness / 2,
+            theta = 0,
+            left = r * Math.cos(theta + 3 / 2 * Math.PI) + this.size / 2,
+            top = r * Math.sin(theta + 3 / 2 * Math.PI) + this.size / 2;
 
         wheelCircle.id = 'color-wheel-circle';
         wheelCircle.style.left = left + 'px';
         wheelCircle.style.top = top + 'px';
 
         this.wheelCircle = wheelCircle;
-
         this.container.appendChild(wheelCircle);
     },
 
@@ -223,15 +211,28 @@ CANVASEVE.Oekaki.prototype = {
     //
 
 
-    _colorWheelArea: function (self) {
-        var centerX = self.centerX;
-        var centerY = self.centerY;
-        var minR = self.wheelInnerRadius;
-        var maxR = self.wheelRadius;
-        var mouseR = self.getDistance(centerX, centerY, clientX, clientY);
+    _colorWheelArea: function () {
+        var isWheelArea = this._isWheelArea();
+        if (isWheelArea) {
+            this._updateWheelCircle();
+        }
+    },
+
+
+    //
+
+
+    _isWheelArea: function () {
+        var centerX = this.centerX,
+            centerY = this.centerY,
+            minR = this.wheelInnerRadius,
+            maxR = this.wheelRadius,
+            mouseR = this.getDistance(centerX, centerY, clientX, clientY);
 
         if (mouseR > minR && maxR > mouseR) {
-            self._updateWheelCircle();
+            return true;
+        } else {
+            return false;
         }
     },
 
@@ -240,11 +241,11 @@ CANVASEVE.Oekaki.prototype = {
 
 
     _updateWheelCircle: function () {
-        var pointer = this.wheelCircle;
-        var r = this.wheelInnerRadius + this.wheelThickness / 2;
-        var theta = this._calcurateTheta();
-        var left = r * Math.cos(theta) + this.size / 2;
-        var top = r * Math.sin(theta) + this.size / 2;
+        var pointer = this.wheelCircle,
+            r = this.wheelInnerRadius + this.wheelThickness / 2,
+            theta = this._calcurateTheta(),
+            left = r * Math.cos(theta) + this.size / 2,
+            top = r * Math.sin(theta) + this.size / 2;
 
         pointer.style.left = left + 'px';
         pointer.style.top = top + 'px';
@@ -255,9 +256,9 @@ CANVASEVE.Oekaki.prototype = {
 
 
     _calcurateTheta: function () {
-        var centerX = this.centerX;
-        var centerY = this.centerY;
-        var theta = calcRadians(clientX - centerX, clientY - centerY);
+        var centerX = this.centerX,
+            centerY = this.centerY,
+            theta = calcRadians(clientX - centerX, clientY - centerY);
 
         return theta;
     },
@@ -271,8 +272,8 @@ CANVASEVE.Oekaki.prototype = {
     drawTriangle: function () {
         var ctx = this.triangleCtx;
 
-        var leftTopX = Math.cos(Math.PI * 2 / 3) * this.triangleRadius;
-        var leftTopY = Math.sin(Math.PI * 2 / 3) * this.triangleRadius;
+        var leftTopX = Math.cos(Math.PI * 2 / 3) * this.triangleRadius,
+            leftTopY = Math.sin(Math.PI * 2 / 3) * this.triangleRadius;
 
         ctx.clearRect(0, 0, this.size, this.size);
         ctx.save();
@@ -285,11 +286,10 @@ CANVASEVE.Oekaki.prototype = {
         ctx.closePath();
         ctx.stroke();
         ctx.clip();
-        ctx.fillStyle = '#000';
         ctx.fillRect(-this.wheelRadius, -this.wheelRadius, this.size, this.size);
 
         var grad0 = ctx.createLinearGradient(this.triangleRadius, 0, leftTopX, 0);
-        var hsla = 'hsla(' + Math.round(0 * (180 / Math.PI)) + ', 100%, 50%, ';
+        var hsla = 'hsla(' + Math.round(this.options.hue * (180 / Math.PI)) + ', 100%, 50%, ';
         grad0.addColorStop(0, hsla + '1)');
         grad0.addColorStop(1, hsla + '0)');
         ctx.fillStyle = grad0;
@@ -309,14 +309,21 @@ CANVASEVE.Oekaki.prototype = {
     //
 
 
+    _getHue: function () {
+
+    },
+
+
+    //
+
+
     _createTriangle: function () {
         var c = document.createElement('canvas');
         c.id = 'color-triangle';
         c.width = c.height = this.size;
-
         var triangleCtx = c.getContext('2d');
-        this.triangleCtx = triangleCtx;
 
+        this.triangleCtx = triangleCtx;
         this.container.appendChild(c);
     },
 
@@ -329,7 +336,6 @@ CANVASEVE.Oekaki.prototype = {
         triangleCircle.id = 'color-triangle-circle';
 
         this.triangleCircle = triangleCircle;
-
         this.container.appendChild(triangleCircle);
     },
 
@@ -337,21 +343,34 @@ CANVASEVE.Oekaki.prototype = {
     //
 
 
-    _colorTriangleArea: function (self) {
-        var centerX = self.centerX;
-        var centerY = self.centerY;
-        var mouseX = clientX - centerX;
-        var mouseY = clientY - centerY;
-        var minX = Math.cos(Math.PI * 2 / 3) * self.triangleRadius;
-        var maxX = self.triangleRadius;
-        var maxY = (-mouseX + maxX) / Math.sqrt(3);
+    _colorTriangleArea: function () {
+        var isTriangleArea = this._isTriangleArea();
+        if (isTriangleArea) {
+            this._updateTriangleCircle();
+        }
+    },
 
-        self._updateTriangleCircle();
+
+    //
+
+
+    _isTriangleArea: function () {
+        var centerX = this.centerX,
+            centerY = this.centerY,
+            mouseX = clientX - centerX,
+            mouseY = clientY - centerY,
+            minX = Math.cos(Math.PI * 2 / 3) * this.triangleRadius,
+            maxX = this.triangleRadius,
+            maxY = (-mouseX + maxX) / Math.sqrt(3);
 
         if (mouseX > minX && maxX > mouseX) {
             if (Math.abs(mouseY) >= 0 && maxY >= Math.abs(mouseY)) {
-
+                return true;
+            } else {
+                return false;
             }
+        } else {
+            return false;
         }
     },
 
@@ -360,34 +379,45 @@ CANVASEVE.Oekaki.prototype = {
 
 
     _updateTriangleCircle: function () {
-        var centerX = this.centerX;
-        var centerY = this.centerY;
-        var mouseX = clientX - centerX;
-        var mouseY = clientY - centerY;
-        var minX = Math.cos(Math.PI * 2 / 3) * this.triangleRadius;
-        var maxX = this.triangleRadius;
-        var minY = (mouseX - maxX) / Math.sqrt(3);
-        var maxY = (-mouseX + maxX) / Math.sqrt(3);
+        var centerX = this.centerX,
+            centerY = this.centerY,
+            mouseX = clientX - centerX,
+            mouseY = clientY - centerY,
+            minX = Math.cos(Math.PI * 2 / 3) * this.triangleRadius,
+            maxX = this.triangleRadius,
+            minY = (mouseX - maxX) / Math.sqrt(3),
+            maxY = (-mouseX + maxX) / Math.sqrt(3);
+        minY = mouseX <= minX ? -Math.sin(Math.PI * 2 / 3) * this.triangleRadius : minY;
+        maxY = mouseX <= minX ? Math.sin(Math.PI * 2 / 3) * this.triangleRadius : maxY;
 
-        console.log('minY', minY, 'maxY', maxY, 'mouseY', mouseY);
+        var pointer = this.triangleCircle,
+            $container = $(this.container),
+            parentNodeX = $container.offset().left,
+            parentNodeY = $container.offset().top,
+            left = clientX - parentNodeX,
+            top = clientY - parentNodeY;
 
-
-        var pointer = this.triangleCircle;
-        var $container = $(this.container);
-        var parentNodeX = $container.offset().left;
-        var parentNodeY = $container.offset().top;
-        var left = clientX - parentNodeX;
-        var top = clientY - parentNodeY;
-
-        if (mouseX > minX && maxX > mouseX) {
+        if (minX < mouseX && mouseX < maxX) {
             pointer.style.left = left + 'px';
             if (mouseY >= maxY) {
-                pointer.style.top = maxY + centerY + 'px';
+                pointer.style.top = maxY + this.size / 2 + 'px';
             } else if (minY >= mouseY) {
-                pointer.style.top = minY + centerY + 'px';
+                pointer.style.top = minY + this.size / 2 + 'px';
             } else {
                 pointer.style.top = top + 'px';
             }
+        } else if (mouseX <= minX) {
+            pointer.style.left = minX + this.size / 2 + 'px';
+            if (maxY <= mouseY) {
+                pointer.style.top = maxY + this.size / 2 + 'px';
+            } else if (mouseY <= minY) {
+                pointer.style.top = minY + this.size / 2 + 'px';
+            } else {
+                pointer.style.top = top + 'px';
+            }
+        } else if (maxX <= mouseX) {
+            pointer.style.left = maxX + this.size / 2 + 'px';
+            pointer.style.top = this.size / 2 + 'px';
         }
     },
 
